8970013aa5cf3354a8acdba7ddb36d425b6685b8
Owen Kiprop <67966931+0xOwen@users.noreply.github.com>
2022-06-08 13:28:03 +0300

    Update server.py

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import socket
 import sys
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 56000        # Port to listen on (non-privileged ports are > 1023)
+PORT = 55000        # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
